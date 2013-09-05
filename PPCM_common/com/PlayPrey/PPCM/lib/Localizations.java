86d7b8fbdb583a6fe6f21056f2aebbc94f26cd1f
Adrian Halfdan Ulland <PlayPrey@Gmail.com>
2013-09-05 17:26:27 +0200

    Added another launguage

--- PPCM_common/com/PlayPrey/PPCM/lib/Localizations.java
+++ PPCM_common/com/PlayPrey/PPCM/lib/Localizations.java
@@ -2,5 +2,5 @@ package com.PlayPrey.PPCM.lib;
 
 public class Localizations {
 
-	public static String[] localeFiles = {References.LANGUAGE_LOCATION + "en_US.xml"};
+	public static String[] localeFiles = {References.LANGUAGE_LOCATION + "en_US.xml", References.LANGUAGE_LOCATION + "nb_NO.xml"};
 }
